290e9570c2df701ce6b697b4126ce3fcbfedbe48
Effect_W <42467046+Effect-Wei@users.noreply.github.com>
2021-03-20 00:29:16 +0800

    Update and rename Generator.py to generator.py

---
+++ generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import cgi
+import cgitb 
+
+# 创建 FieldStorage 的实例化
+form = cgi.FieldStorage() 
+
+# 获取数据
+server_name = form.getvalue('name')
+server_link = form.getvalue('link')
+
+print ("Content-type:text/html")
+print ()
+print (name, " ", link)
