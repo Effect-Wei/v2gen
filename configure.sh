c943c2f9cfdc05c411396defde8248444fb205a6
Effect_W <42467046+Effect-Wei@users.noreply.github.com>
2021-03-22 16:03:03 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/bin/bash -c "envsubst < /etc/nginx/conf.d/v2gen.template > /etc/nginx/conf.d/v2gen.conf"
-service nginx restart
+/bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/v2gen.template > /etc/nginx/conf.d/v2gen.conf" && nginx -g 'daemon off;'
+#service nginx restart
 service fcgiwrap restart
