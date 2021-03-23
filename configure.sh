5891cd7d565f67f895d5e964835e3908f0af5f5e
Effect_W <42467046+Effect-Wei@users.noreply.github.com>
2021-03-24 00:36:37 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-rm -rf /etc/nginx/sites-enabled
-/bin/bash -c "echo listen $PORT default_server\; > /etc/nginx/conf.d/listen"
-service nginx restart
-service fcgiwrap start
+apt update
+apt install python3
