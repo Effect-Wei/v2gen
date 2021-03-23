426654e3fd417f886113b35563135c2bc8209dd1
Effect_W <42467046+Effect-Wei@users.noreply.github.com>
2021-03-23 20:05:23 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,4 +3,4 @@
 rm -rf /etc/nginx/sites-enabled
 /bin/bash -c "echo listen $PORT default_server\; > /etc/nginx/conf.d/listen"
 service nginx restart
-service fcgiwrap restart
+service fcgiwrap start
